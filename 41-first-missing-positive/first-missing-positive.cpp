8b0edb6df4f9f02ee321b79ed073a66eaa9cb578
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-03-26 22:56:42 +0530

    Time: 24 ms (98.59%) | Memory: 43.3 MB (98.91%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
        vector<bool>seen(nums.size()+1,false);
        for(int num: nums){
         if(num>0 && num<=nums.size()){
