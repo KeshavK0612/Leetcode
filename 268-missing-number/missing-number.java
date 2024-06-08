1b707ddce5c10bf0711d4c92d6aa3a808628076e
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-06-09 01:39:56 +0530

    Time: 1 ms (40.28%) | Memory: 45.3 MB (33.14%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+class Solution {
+    public int missingNumber(int[] nums) {
+        int ans = nums.length;
+        for(int i = 0;i<nums.length;i++){
+            ans += i - nums[i];
+        }
+        return ans;
+    }
+}
