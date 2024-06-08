0f5df5c9450a0c64b8dd6c17319f185f1ee06a82
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-06-08 23:31:42 +0530

    Time: 3 ms (26.76%) | Memory: 46.8 MB (8.78%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,19 +1,14 @@
-import java.util.ArrayList;
 class Solution {
     public void moveZeroes(int[] nums) {
-        ArrayList<Integer> temp = new ArrayList<Integer>();
-        int n = nums.length;
-        for(int i = 0; i<n;i++){
-            if(nums[i]!=0){
-                temp.add(nums[i]);
+        int left = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
             }
-        }
-        for(int i = 0;i<temp.size();i++){
-            nums[i] = temp.get(i);
-        }
-        int nonz = temp.size();
-        for(int i = nonz; i<n ; i++){
-            nums[i] = 0;
-        }
+        }        
     }
 }
