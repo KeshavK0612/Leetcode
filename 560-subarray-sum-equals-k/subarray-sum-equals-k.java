e0f22d59fc5e42494c064973a2adc16e55fd1813
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-06-11 22:00:05 +0530

    Time: 24 ms (74.30%) | Memory: 45.9 MB (83.99%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -7,22 +7,18 @@ class Solution {
         Map<Integer, Integer> mpp = new HashMap<>();
         int preSum = 0, cnt = 0;
 
-        // Initialize the map with the base case
         mpp.put(0, 1); 
         
-        // Traverse through the array
         for (int i = 0; i < n; i++) {
-            preSum += nums[i]; // Update the prefix sum
+            preSum += nums[i]; 
 
-            // Calculate the value needed to form the sum k
             int remove = preSum - k;
 
-            // Add to count the number of times the needed value has been seen
             cnt += mpp.getOrDefault(remove, 0);
 
-            // Update the map with the current prefix sum
+
             mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
         }
-        return cnt; // Return the total count of subarrays with sum k
+        return cnt; 
     }
 }
