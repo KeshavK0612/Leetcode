8c6f5d46e7bda38623d625b6b4ffad0b15521fc7
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-07-15 01:22:34 +0530

    Time: 89 ms (8.77%) | Memory: 45.8 MB (84.16%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -3,21 +3,22 @@ import java.util.Arrays;
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n];
-        for (int i = 0; i < n; i++) {
-            ans[i] = -1;
-        }
+        int[] res = new int[n];
+        
+        // Initialize result array with -1
+        Arrays.fill(res, -1);
 
+        // Iterate through each element to find its next greater element
         for (int i = 0; i < n; i++) {
+            // Start from the next element and loop until the original element
             for (int j = 1; j < n; j++) {
                 if (nums[(i + j) % n] > nums[i]) {
-                    ans[i] = nums[(i + j) % n];
+                    res[i] = nums[(i + j) % n];
                     break;
                 }
             }
         }
-
-        return ans;
+        
+        return res;
     }
-
 }
