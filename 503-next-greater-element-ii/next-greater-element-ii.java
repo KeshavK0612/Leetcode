660a10a7a103cd5fcb7a7e150b28344848971c11
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-07-15 01:18:32 +0530

    Time: 90 ms (6.68%) | Memory: 45.6 MB (94.69%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -3,22 +3,19 @@ import java.util.Arrays;
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         int n = nums.length;
-        int[] res = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            res[i] = -1;
-        }
+        int[] ans = new int[n];
+        Arrays.fill(ans, -1); 
 
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < n; j++) {
                 if (nums[(i + j) % n] > nums[i]) {
-                    res[i] = nums[(i + j) % n];
+                    ans[i] = nums[(i + j) % n];
                     break;
                 }
             }
         }
 
-        return res;
+        return ans;
     }
 
 }
