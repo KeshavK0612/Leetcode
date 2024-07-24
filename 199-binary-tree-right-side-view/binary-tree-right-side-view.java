f1f66df2592b5d7fd914cca7bcc9e7b45e16bd2a
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-07-24 19:52:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (85.57%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -15,22 +15,16 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if (root == null) return result;
+        List<Integer> arr = new ArrayList<>();
+        func(0,arr,root);
+        return arr;
+    }
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        while (!queue.isEmpty()) {
-            int n = queue.size();
-            List<Integer> values = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                TreeNode node = queue.poll();
-                if (node.left != null) queue.add(node.left);
-                if (node.right != null) queue.add(node.right);
-                values.add(node.val);
-            }
-            result.add(values.get(values.size() - 1));
-        }
-        return result;
+    public void func(int level, List<Integer> arr, TreeNode root){
+        if(root == null) return;
+        if (level == arr.size()) arr.add(root.val);
+        else arr.set(level, root.val);
+        func(level+1,arr,root.left);
+        func(level+1,arr,root.right);
     }
 }
