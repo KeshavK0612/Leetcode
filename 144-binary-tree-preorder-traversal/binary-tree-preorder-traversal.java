09a7893a08c47afc0c4e1c37b52a1b076eb63670
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-07-24 23:47:25 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (21.34%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -14,16 +14,18 @@
  * }
  */
 class Solution {
-    List<Integer> pre = new ArrayList<>();
+    
     public List<Integer> preorderTraversal(TreeNode root) {
-        preOrder(root);
+        List<Integer> pre = new ArrayList<>();
+        if(root==null) return pre;
+        Stack <TreeNode> st = new Stack <TreeNode>();
+        st.push(root);
+        while(!st.isEmpty()){
+            root = st.pop();
+            pre.add(root.val);
+            if(root.right!=null) st.push(root.right);
+            if(root.left!= null) st.push(root.left);
+        }
         return pre;
-    }
-
-    private void preOrder(TreeNode node) {
-        if(node==null) return;
-        pre.add(node.val);
-        preOrder(node.left);
-        preOrder(node.right);
     }
 }
