feddf91bd67279b9c423746ca9f59ab1ccd2b2f5
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-07-28 02:26:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (68.08%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,19 +14,20 @@
  * }
  */
 class Solution {
+    private TreeNode prev = null;
+
     public boolean isValidBST(TreeNode root) {
-        List<Integer> arr = new ArrayList<>();
-        inorder(root, arr);
-        for(int i = 0; i<arr.size()-1;i++){
-            if(arr.get(i)>=arr.get(i+1)) return false;
-        }
-        return true;
+        return inorder(root);
     }
 
-    public void inorder(TreeNode root,List<Integer> arr){
-        if(root==null) return;
-        inorder(root.left,arr);
-        arr.add(root.val);
-        inorder(root.right,arr);
+    private boolean inorder(TreeNode root) {
+        if (root == null) return true;
+
+        if (!inorder(root.left)) return false;
+
+        if (prev != null && root.val <= prev.val) return false;
+        prev = root;
+
+        return inorder(root.right);
     }
 }
