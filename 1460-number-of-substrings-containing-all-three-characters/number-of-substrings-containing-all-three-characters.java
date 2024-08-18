ab61a13361d8ac683d433fdd3ee29f6638f0a8c0
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-08-18 15:14:35 +0530

    Time: 12 ms (66.82%) | Memory: 44.5 MB (64.76%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,26 +1,15 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int[] count = new int[3]; // To count occurrences of 'a', 'b', 'c'
-        int res = 0; // To store the result
-        int i = 0; // Left pointer of the window
-
-        // Iterate through the string with the right pointer
-        for (int j = 0; j < s.length(); j++) {
-            // Increment the count of the current character
-            count[s.charAt(j) - 'a']++;
-
-            // Shrink the window from the left while it remains valid
-            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
-                // All substrings starting from index i to j are valid
-                res += s.length() - j;
-                
-                // Decrement the count of the character at the left pointer
-                count[s.charAt(i) - 'a']--;
-                
-                // Move the left pointer to the right
-                i++;
+        int n = s.length();
+        int lastseen[] = { -1, -1, -1 };
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            lastseen[s.charAt(i) - 'a'] = i;
+            if (lastseen[0] != -1 && lastseen[1] != -1 && lastseen[2] != -1) {
+                cnt = cnt + (1 + Math.min(Math.min(lastseen[0], lastseen[1]), lastseen[2]));
             }
+
         }
-        return res;
+        return cnt;
     }
 }
