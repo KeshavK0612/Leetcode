b50e4b1b249f8f2d64c1054a4433c5200e99c255
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-08-20 14:30:07 +0530

    Time: 1 ms (98.68%) | Memory: 45.1 MB (8.97%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,35 +1,24 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> ans = new ArrayList<>();
-        int start = newInterval[0], end = newInterval[1];
-        boolean inserted = false;
-        
-        for (int[] inv : intervals) {
-            int cstart = inv[0], cend = inv[1];
-            
-            if (cend < start || inserted) {
-                ans.add(new int[]{cstart, cend});
-                continue;
-            }
-            
-            start = Math.min(start, cstart);
-            if (end < cstart) {
-                ans.add(new int[]{start, end});
-                ans.add(new int[]{cstart, cend});
-                inserted = true;
-                continue;
-            }
-            
-            if (end <= cend) {
-                ans.add(new int[]{start, cend});
-                inserted = true;
-            }
-        }
-        
-        if (!inserted) {
-            ans.add(new int[]{start, end});
-        }
-        
-        return ans.toArray(new int[ans.size()][]);
+        int n = intervals.length;
+        List <int[]> res = new ArrayList<>();
+        int i = 0;
+        while (i < n && intervals[i][1] < newInterval[0]) {
+        res.add(intervals[i]);
+        i++;
+    }
+
+    while (i < n && intervals[i][0] <= newInterval[1]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        i++;
+    }
+    res.add(newInterval);
+
+    while (i < n) {
+        res.add(intervals[i]);
+        i++;
+    }
+        return res.toArray(new int[res.size()][]);
     }
 }
