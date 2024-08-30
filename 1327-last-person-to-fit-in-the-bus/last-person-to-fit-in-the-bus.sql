fa9cc3ba54e45f18df652061b131bc3730213a91
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2024-08-31 01:20:29 +0530

    Time: 1504 ms (36.60%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,5 +1,5 @@
 select q1.person_name from queue q1 
-join queue q2 on q1.turn >= q2.turn
+join queue q2 on q2.turn <= q1.turn
 group by q1.turn
 having sum(q2.weight) <= 1000
 order by sum(q2.weight) desc
