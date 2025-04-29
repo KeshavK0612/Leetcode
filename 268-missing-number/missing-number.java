3d33db29d2ac7cece4b463d96f48a32296b27ef0
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2025-04-29 09:06:00 +0530

    Time: 5 ms (25.73%) | Memory: 45.8 MB (13.05%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,16 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int xor = 0;
-        int n = nums.length;
-
-        // XOR all indices and values in the array
-        for (int i = 0; i < n; i++) {
-            xor ^= i ^ nums[i];
+        Arrays.sort(nums);
+        int ans = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i]!=i) return i;
         }
-
-        // XOR the last index n
-        xor ^= n;
-
-        return xor;
+        return nums.length;
     }
 }
