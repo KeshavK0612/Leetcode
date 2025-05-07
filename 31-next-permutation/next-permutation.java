c6edf833419170070908e3c72e04beb266ad5ecf
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2025-05-07 10:26:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (56.17%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,43 +1,39 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int ind = -1;
         int n = nums.length;
+        int k = -1;
 
-        // Step 1: Find the first index from the end where nums[i] < nums[i + 1]
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                ind = i;
+        for (int i = n-2; i>=0; i--){
+            if(nums[i]<nums[i+1]){
+                k=i;
                 break;
             }
         }
 
-        // Step 2: If no such index is found, reverse the entire array
-        if (ind == -1) {
-            reverseArray(nums, 0, n - 1);
-        } else {
-            // Step 3: Find the next greater element from the end and swap it with nums[ind]
-            for (int i = n - 1; i > ind; i--) {
-                if (nums[i] > nums[ind]) {
-                    int temp = nums[i];
-                    nums[i] = nums[ind];
-                    nums[ind] = temp;
-                    break;
-                }
-            }
-            // Step 4: Reverse the array from ind+1 to the end
-            reverseArray(nums, ind + 1, n - 1);
+        if(k==-1){
+            reverse(nums,0,n-1);
+            return;
         }
+
+        for(int i = n-1; i>k; i--){
+            if(nums[i]>nums[k]){
+                swap(nums, i, k);
+                break;
+            }
+        }
+
+        reverse(nums, k+1, n-1);
     }
 
-    // Utility function to reverse the array from start to end
-    static void reverseArray(int arr[], int start, int end) {
-        while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end){
+        while(start < end){
+            swap(nums, start++, end--);
         }
     }
-    
 }
