d70d4e63e0c17bc56e38ecd33c1a7e8d54453c4e
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2025-05-08 10:19:14 +0530

    Time: 23 ms (92.37%) | Memory: 46.5 MB (54.78%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -4,7 +4,7 @@ class Solution {
         int n = nums.length;
         int sum = 0;
         int count = 0;
-        map.put(sum,1);
+        map.put(0,1);
 
         for(int i = 0; i<n; i++){
             sum+=nums[i];
