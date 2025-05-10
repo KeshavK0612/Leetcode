97fd16291ed560bab32d9da23c4fd0aa64412be9
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2025-05-10 14:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (51.81%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -3,6 +3,7 @@ class Solution {
         String prefix = strs[0];
         for(int index=1;index<strs.length;index++){
             while(strs[index].indexOf(prefix) != 0){
+                if (prefix.isEmpty()) return "";
                 prefix=prefix.substring(0,prefix.length()-1);
             }
         }
