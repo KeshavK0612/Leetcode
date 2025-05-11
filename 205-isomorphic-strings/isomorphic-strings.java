4c65d10a17dae308c994d8ad131207eb018588e6
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2025-05-11 09:17:26 +0530

    Time: 7 ms (86.84%) | Memory: 43 MB (11.74%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,29 +1,23 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) return false;
-
-        Map<Character, Character> mapST = new HashMap<>();
-        Map<Character, Character> mapTS = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char cs = s.charAt(i);
-            char ct = t.charAt(i);
-
-            // Check if mapping exists and is consistent
-            if (mapST.containsKey(cs)) {
-                if (mapST.get(cs) != ct) return false;
-            } else {
-                mapST.put(cs, ct);
-            }
-
-            // Check reverse mapping (t → s) to avoid duplicates
-            if (mapTS.containsKey(ct)) {
-                if (mapTS.get(ct) != cs) return false;
-            } else {
-                mapTS.put(ct, cs);
-            }
+        int[] indexS = new int[200]; 
+        int[] indexT = new int[200]; 
+        
+        int len = s.length();
+        
+        if(len != t.length()) {
+            return false;
         }
-
+        
+        for(int i = 0; i < len; i++) {
+            if(indexS[s.charAt(i)] != indexT[t.charAt(i)]) {
+                return false; 
+            }
+            
+            indexS[s.charAt(i)] = i + 1;
+            indexT[t.charAt(i)] = i + 1;
+        }
+        
         return true;
     }
 }
