120121b0ddfd21b04083617a66be86d5137e1a3b
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2026-02-13 14:24:02 +0530

    Time: 469 ms (16.82%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,1 +1,1 @@
-SELECT MAX(num) AS num  FROM MyNumbers WHERE num IN (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(*) = 1);
+SELECT Max(num) AS num FROM MyNumbers WHERE num NOT IN (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num)>1);
