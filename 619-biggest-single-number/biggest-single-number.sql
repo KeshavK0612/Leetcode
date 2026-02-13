832ad372b572b846311232a2b38e072a552650dd
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2026-02-13 14:22:59 +0530

    Time: 413 ms (52.56%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,5 +1,1 @@
-# Write your MySQL query statement below
-SELECT Max(num) AS num FROM MyNumbers 
-WHERE num NOT IN (SELECT num FROM MyNumbers 
-GROUP BY num 
-HAVING COUNT(num)>1);
+SELECT MAX(num) AS num FROM (SELECT num FROM MyNumbers GROUP BY num HAVING COUNT(num) = 1 ) NEW;
