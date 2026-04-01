1b253df9d062f553c5f024cc8f25091549a2c9a8
KeshavK0612 <130403119+KeshavK0612@users.noreply.github.com>
2026-04-01 11:13:42 +0530

    Time: 640 ms (47.99%) | Memory: 0B (100.00%) - LeetSync

--- 1811-fix-names-in-a-table/fix-names-in-a-table.sql
+++ 1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
-SELECT user_id, 
-       CONCAT(UCASE(LEFT(name, 1)), LCASE(SUBSTRING(name, 2))) AS name
-FROM Users
-ORDER BY user_id
+select user_id, concat(upper(substring(name,1,1)), lower(substring(name,2))) as name 
+from users
+order by user_id
